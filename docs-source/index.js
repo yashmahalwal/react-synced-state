796ad1e608b8d9bec501ee099a41d4639bf8ac08
Yash Mahalwal <yashmahalwal@gmail.com>
2022-01-16 14:10:41 +0530

    fix(github-actions): deploy github page

--- docs-source/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-const main = document.createElement("main");
-document.body.prepend(main);
-ReactDOM.render(React.createElement("h1", null, "Hello, World!"), main);
-//# sourceMappingURL=index.js.map
--- docs-source/index.tsx
+++ docs-source/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../build/index";
 import App from "./App";
 const main = document.createElement("main");
 document.body.prepend(main);
