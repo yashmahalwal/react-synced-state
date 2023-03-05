f8f1082f8a89dfb317da4b456409794b550565af
Yash Mahalwal <yashmahalwal@gmail.com>
2023-03-05 17:28:05 +0530

    test(synced-state-hook/priority): added the test case checking priority queueing integration with the hook

--- tests/use-synced-state.test.tsx
+++ tests/use-synced-state.test.tsx
@@ -5,6 +5,7 @@ import { SyncedStateProvider } from "../src";
 import ControlledModals from "../docs-source/CodeSamples/ControlledModals";
 import { ProviderWrapper } from "./ProviderWrapper";
 import QueueDemo from "../docs-source/CodeSamples/QueueDemo";
+import PriorityQueueDemo from "../docs-source/CodeSamples/PriorityQueueDemo";
 
 describe("Use synced state hook", () => {
   it("Works as normal state hook if only single component used", async () => {
@@ -42,7 +43,7 @@ describe("Use synced state hook", () => {
     expect(entry).toHaveTextContent("100");
   });
 
-  it("It allows only one state update at once", async () => {
+  it("Allows only one state update at once", async () => {
     render(<ControlledModals />, { wrapper: ProviderWrapper });
 
     const getDialog1 = () => screen.queryByText("Dialog 1 title");
@@ -89,7 +90,7 @@ describe("Use synced state hook", () => {
     expect(getDialog2()).toBeFalsy();
   });
 
-  it("It processes state updates in FIFO oder", async () => {
+  it("Processes state updates in FIFO oder", async () => {
     render(<QueueDemo />, { wrapper: ProviderWrapper });
 
     const getCheckbox1 = () => screen.getByLabelText("State 1") as HTMLInputElement;
@@ -178,5 +179,90 @@ describe("Use synced state hook", () => {
     expect(getSyncedCheckbox1()).not.toBeChecked();
     expect(getSyncedCheckbox2()).toBeChecked();
     expect(getSyncedCheckbox3()).not.toBeChecked();
+  });
+
+  it("Processes state updates along with priority", async () => {
+    render(<PriorityQueueDemo />, { wrapper: ProviderWrapper });
+
+    const getCheckbox1 = () => screen.getByLabelText("State 1") as HTMLInputElement;
+    const getCheckbox2 = () => screen.getByLabelText("State 2") as HTMLInputElement;
+    const getCheckbox3 = () => screen.getByLabelText("State 3") as HTMLInputElement;
+
+    const getSyncedCheckbox1 = () => screen.getByLabelText("Synced State 1") as HTMLInputElement;
+    const getSyncedCheckbox2 = () => screen.getByLabelText("Synced State 2") as HTMLInputElement;
+    const getSyncedCheckbox3 = () => screen.getByLabelText("Synced State 3") as HTMLInputElement;
+
+    // For three boolean variables (values represented by a 3 bit number)
+    async function makeValue(state1: boolean, state2: boolean, state3: boolean) {
+      const checkbox1 = getCheckbox1();
+      const checkbox2 = getCheckbox2();
+      const checkbox3 = getCheckbox3();
+
+      if (checkbox1.checked !== state1) {
+        await userEvent.click(checkbox1);
+      }
+
+      if (checkbox2.checked !== state2) {
+        await userEvent.click(checkbox2);
+      }
+
+      if (checkbox3.checked !== state3) {
+        await userEvent.click(checkbox3);
+      }
+    }
+
+    // 000 - None checked
+    await makeValue(false, false, false);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).not.toBeChecked();
+
+    // 100 - 1
+    await makeValue(true, false, false);
+    expect(getSyncedCheckbox1()).toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).not.toBeChecked();
+
+    // 101 - 3
+    await makeValue(true, false, true);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).toBeChecked();
+
+    // 111 - 3
+    await makeValue(true, true, true);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).toBeChecked();
+
+    // 110 - 2
+    await makeValue(true, true, false);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).toBeChecked();
+    expect(getSyncedCheckbox3()).not.toBeChecked();
+
+    // 010 - 2
+    await makeValue(false, true, false);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).toBeChecked();
+    expect(getSyncedCheckbox3()).not.toBeChecked();
+
+    // 011 - 3
+    await makeValue(false, true, true);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).toBeChecked();
+
+    // 001 - 3
+    await makeValue(false, false, true);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).toBeChecked();
+
+    // 101 - 3
+    await makeValue(true, false, true);
+    expect(getSyncedCheckbox1()).not.toBeChecked();
+    expect(getSyncedCheckbox2()).not.toBeChecked();
+    expect(getSyncedCheckbox3()).toBeChecked();
   });
 });
